7b234633acd289fb90a9567ee49e4d42c194831a
Mateusz Mikolajewski <mateusz.mikolajewski@gmail.com>
2018-03-21 15:27:48 +0100

    added mapping of lion dna to the object attributes

--- src/store/utils.js
+++ src/store/utils.js
@@ -1,7 +1,23 @@
 import firstNames from 'random-name/first-names.json'
+import middleNames from 'random-name/middle-names.json'
+import lastNames from 'random-name/names.json'
+import places from 'random-name/places.json'
 
 export function mapLionProperties (lion) {
   // TODO: From ETH Address to
-  console.log(firstNames[lion.dna[0]])
-  return Object.assign({}, lion, {firstName: firstNames[lion.dna[0]]})
+  console.log(lion.dna)
+  return Object.assign(
+    {}, lion, {
+      firstName: firstNames[lion.dna.substring(0, 5) % firstNames.length],
+      middleName: middleNames[lion.dna.substring(5, 8) % middleNames.length],
+      lastName: lastNames[lion.dna.substring(3, 7) % lastNames.length],
+      birthPlace: places[lion.dna.substring(7, 10) % places.length],
+      maneType: lion.dna[10],
+      tailType: lion.dna[11],
+      eyesType: lion.dna[12],
+      eyesColour: lion.dna[13],
+      furColour: lion.dna[14],
+      maneColour: lion.dna[15]
+    }
+  )
 }
